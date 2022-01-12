b407b82d5cefb8baf9c9df4ce74c56e308821535
Lennart Bruns <lennart.bruns@uni-oldenburg.de>
2022-01-12 09:48:02 +0100

    Fix epsilon update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from CheckpointManager import CheckpointManager
 from Simulator import CarEnvironment
 from Simulator import WIDTH, HEIGHT
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # TODO testen, ob bei den Schwarz/ weiß bildern die Werte zwischen 0 und 1 sind. Wenn ja müssen wir die Bilder nicht mehr durch 255 teilen
 
@@ -18,8 +18,6 @@ import time
 import numpy as np
 
 from threading import Thread
-
-from tqdm import tqdm
 
 # ==============================================================================
 # -- Find CARLA module ---------------------------------------------------------
@@ -46,19 +44,20 @@ import carla
 # -- Defining Constants --------------------------------------------------------
 # ==============================================================================
 
-PORT = "127.0.0.1"
-HOST = 2000
+PORT = 2000
+HOST = "127.0.0.1"
 
 
 
 MEMORY_FRACTION = 0.4
-MIN_REWARD = -1
+MIN_REWARD = 4
 
 EPISODES = 100
 
 epsilon = 1
 EPSILON_DECAY = 0.95  ## 0.9975 99975
 MIN_EPSILON = 0.001
+MIN_EPSILON_2 = 0.01
 
 AGGREGATE_STATS_EVERY = 10
 
@@ -78,12 +77,13 @@ def learn_loop():
 
     try:
         # connect to the CARLA simulator
-        client = carla.Client(PORT, HOST)
-        client.set_timeout(5.0)
-
-        client.load_world("Town02_Opt")
         # TODO layer unloaden, in dem kleine Sachen sind, die Kollisionen verursachen könnten, die wir nicht gebrauchen können.
-
+        client = carla.Client(HOST, PORT)
+        client.set_timeout(5.0)
+        map_name = client.get_world().get_map().name
+        if map_name != "Town02_Opt":
+            client.load_world("Town02_Opt")
+            time.sleep(1)
         sim_world = client.get_world()
 
         # create car environment in the simulator and our Reinforcement Learning agent
@@ -102,9 +102,7 @@ def learn_loop():
             agent.get_qs(start_state)
 
         episode = 0
-
         while True:
-        #for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
             episode += 1
             # Update tensorboard step every episode
             agent.tensorboard.step = episode
@@ -123,35 +121,35 @@ def learn_loop():
 
             # Play for given number of seconds only
             while True:
-
                 # get fitting action from Q table for the current state, or select one at random
-                if np.random.random() > epsilon:
-                    action = np.argmax(agent.get_qs(current_state))
-                    # print(agent.get_qs(current_state))
+                if current_state[3] == 0:
+                    action = 1
+                elif np.random.random() > epsilon:
+                    qs = agent.get_qs(current_state)
+                    action = np.argmax(qs)
                 else:
                     action = np.random.randint(0, 9)
                     time.sleep(1 / FPS)
 
-                action = action % 3
+                #action = action % 3
                 # execute the action in the environment
                 new_state, reward, done, _ = car_environment.step(action)
 
                 # Transform new continuous state to new discrete state and count reward
                 episode_reward += reward
-
                 # Every step we update replay memory
                 agent.update_replay_memory((current_state, action, reward, new_state, done))
 
                 current_state = new_state
                 step += 1
-
+                
                 if done:
                     break
 
             # End of episode - destroy agents
             car_environment.destroy()
-
             # Append episode reward to a list and log stats (every given number of episodes)
+            
             ep_rewards.append(episode_reward)
             if not episode % AGGREGATE_STATS_EVERY or episode == 1:
                 average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:]) / len(ep_rewards[-AGGREGATE_STATS_EVERY:])
@@ -169,12 +167,14 @@ def learn_loop():
             if epsilon > MIN_EPSILON:
                 epsilon *= EPSILON_DECAY
                 epsilon = max(MIN_EPSILON, epsilon)
-
+            if epsilon < MIN_EPSILON_2:
+                epsilon = 0.5
             print(str(episode_reward) + " :Reward|Epsilon: " + str(epsilon))
 
             if episode % 1000 == 0:
                 agent.model.save(
                     f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+            
 
         # Set termination flag for training thread and wait for it to finish
         agent.terminate = True
@@ -188,6 +188,26 @@ def learn_loop():
 
         # pygame.quit()
 
+
+def action_to_s(action):
+    if action == 0:
+        return "accelerate left"
+    elif action == 1:
+        return "accelerate straight"
+    elif action == 2:
+        return "accelerate right"
+    elif action == 3:
+        return "steady left"
+    elif action == 4:
+        return "steady straight"
+    elif action == 5:
+        return "steady right"
+    elif action == 6:
+        return "brake left"
+    elif action == 7:
+        return "brake straight"
+    elif action == 8:
+        return "brake right"
 
 # ==============================================================================
 # -- main() --------------------------------------------------------------
@@ -207,7 +227,6 @@ def main():
     # start the learning process
     try:
         learn_loop()
-
     except KeyboardInterrupt:
         print('\nCancelled by user.')
 
